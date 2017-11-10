f3d4cb1802b9066f94859a06b19013173d00c71a
vanminh <vanminh020698@gmail.com>
2017-11-10 17:16:22 +0700

    update

--- Parkour/src/backgroundLayer.js
+++ Parkour/src/backgroundLayer.js
@@ -1,5 +1,4 @@
 var BackgroundLayer = cc.Layer.extend({
-<<<<<<< HEAD
     map00:null,
     map01:null,
     mapWidth:0,
@@ -120,51 +119,4 @@ var BackgroundLayer = cc.Layer.extend({
 
     }
 });
-=======
-	map00:null,
-	map01:null,
-	mapWidth:0,
-	mapIndex:0,// to determine map which the runner is in
-	ctor: function () {
-		this._super();
-		this.init();
-		this.scheduleUpdate();
-	},
-	init: function() {
-		this._super();
-		this.map00 = new cc.TMXTiledMap(res.map00_tmx);
-		this.addChild(this.map00);
-		this.mapWidth = this.map00.getContentSize().width;
-		this.map01 = new cc.TMXTiledMap(res.map01_tmx);
-		this.map01.setPosition(cc.p(this.mapWidth, 0));
-		this.addChild(this.map01);
-	},
-	checkAndReload:function (eyeX) {
-
-		//append screen
-		var newMapIndex = parseInt(eyeX / this.mapWidth);
-		console.log(newMapIndex);
-		if(newMapIndex == this.mapIndex) {
-			return false;
-		}
-		//the runner is in map01
-		if(newMapIndex % 2 == 1) {
-			console.log("the runner is in map01");
-			this.map00.setPositionX(this.mapWidth*2);
-		} else {
-			console.log("the runner is in map00");
-			this.map01.setPositionX(this.mapWidth*2);//the runner is in map00
-		}
-		mapIndex = newMapIndex;
-		return true;
-	},
-
-	update:function (dt) {
-        var animationLayer = this.getParent().getChildByTag(TagOfLayer.Animation);
-        var eyeX = animationLayer.getEyeX();
-       // this.checkAndReload(eyeX);
-
-    }
-
-});
->>>>>>> fa7aba864963956f7375bf18f8d51fde0678be54
+	
